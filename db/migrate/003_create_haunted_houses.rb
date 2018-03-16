5fc50da2741d917a395a42ad3e8603ff9dcb607a
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-16 19:49:14 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |q|
+      q.string :name
+      q.string :location
+      q.string :theme
+      q.string :description
+      q.float :price
+      q.boolean :family_friendly
+      q.datetime :opening_date
+      q.datetime :closing_date
+    end
+  end
+end
