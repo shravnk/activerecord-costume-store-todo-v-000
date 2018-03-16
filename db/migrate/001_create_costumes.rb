fcfcb02b81c1a70ad4a69148d40f1dfdbacc5840
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-16 19:08:39 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,6 +13,9 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |c|
       c.string :name
       c.float :price
+      c.string :image_url
+      c.string :size
+      c.timestamps
     end
   end
 end
